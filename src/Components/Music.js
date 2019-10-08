b8c6db922ce88c7852ed64e311830eda22f89763
Hunter Redding <hunterred50@gmail.com>
2019-10-08 15:35:45 -0500

    update

--- src/Components/Music.js
+++ src/Components/Music.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
-import Transition from 'react-bootstrap/Transition';
+// import Transition from 'react-bootstrap/Transition';
 
 class Music extends Component {
   render() {
     return (
-      <Transition>
+      // <Transition>
       <div className="container" id="music">
         <h1>Music</h1><p></p>
-      <iframe src="https://open.spotify.com/embed/album/2K8GFA0sQhnHFAZQxvgDeF" width="75%" height="390" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+      <iframe title="Lock the Gate" src="https://open.spotify.com/embed/album/2K8GFA0sQhnHFAZQxvgDeF" width="75%" height="390" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
       <p></p>
-      <iframe src="https://open.spotify.com/embed/album/2QZUdZ6SubPpVqcCfxZLTd" width="75%" height="235" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+      <iframe title="Waterfall" src="https://open.spotify.com/embed/album/2QZUdZ6SubPpVqcCfxZLTd" width="75%" height="235" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
       </div>
-      </Transition>
+      // </Transition>
     )
   }
 }
