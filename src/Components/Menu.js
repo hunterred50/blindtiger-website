fbc4860dfa0bdec801803596b33d3b070fdf403d
Hunter Redding <hunterred50@gmail.com>
2020-01-20 12:19:28 -0500

    reverting imports

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import Navbar from 'react-bootstrap/src/Navbar';
-import Nav from 'react-bootstrap/src/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 // import NavDropdown from 'react-bootstrap/NavDropdown';
 import '../App.css';
 
