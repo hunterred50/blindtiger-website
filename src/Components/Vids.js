f2ed7b10b88d342992ff0d178fffb79aad4bbe3c
Hunter Redding <hunterred50@gmail.com>
2020-01-22 00:06:05 -0500

    Added vids

--- src/Components/Vids.js
+++ src/Components/Vids.js
@@ -12,7 +12,15 @@ class Vids extends Component {
     return (
       <div className="container" id="vids" style={{paddingBottom: '70px'}}>
         <h1>Vids</h1>
+        <p style={{paddingTop:'20px'}}></p>
         <iframe width="560" height="315" src="https://www.youtube.com/embed/XLlM3NzrcKU" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen />
+        <p style={{paddingTop:'20px'}}></p>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/MkTeVukUPdw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen />
+        <p style={{paddingTop:'20px'}}></p>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/_bOE20PcIfI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen />
+        <p style={{paddingTop:'20px'}}></p>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/m1k8TS0Nsjo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen />
+
       </div>
     )
   }
