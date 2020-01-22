9b7f2a6088f403fd083ed5e9e37071cfab01c264
Hunter Redding <hunterred50@gmail.com>
2020-01-21 23:25:23 -0500

    menu scroll fade

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -4,24 +4,45 @@ import Nav from 'react-bootstrap/Nav';
 // import NavDropdown from 'react-bootstrap/NavDropdown';
 import '../App.css';
 
+
 class Menu extends Component {
+  constructor() {
+    super()
+    this.state = {
+      currentScrollHeight: ''
+    }
+  }
+
+  componentDidMount () {      
+    window.onscroll = () => {
+     const newScrollHeight = Math.ceil(window.scrollY / 50) *50;
+     if (this.state.currentScrollHeight != newScrollHeight){
+         this.setState({currentScrollHeight: newScrollHeight})
+     }
+   }
+ }
+
   render() {
+    const opacity = Math.min(100 / this.state.currentScrollHeight  , 1);
+
     return (
-      <Navbar collapseOnSelect fixed="top" expand="sm" variant="dark">
-        <Navbar.Brand href="#home"><img src="https://gdurl.com/HWaN" alt="" height="25em"/>Blind Tiger</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav justify className="navbar" justify-content="end">
-            <Nav.Link className="navtext" href="#music">MUSIC</Nav.Link>
-            <Nav.Link className="navtext" href="#tour">TOUR</Nav.Link>
-            <Nav.Link className="navtext" href="#merch">MERCH</Nav.Link>
-            {/* <NavDropdown alignRight title="Inspiration" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Youtube Poop Playlist</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Musical Inspiration</NavDropdown.Item>
-            </NavDropdown> */}
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+      <div style={{opacity}} id='Menu'>
+        <Navbar collapseOnSelect fixed="top" expand="sm" variant="dark">
+          <Navbar.Brand href="#home"><img src="https://gdurl.com/HWaN" alt="" height="25em"/>Blind Tiger</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav justify className="navbar" justify-content="end">
+              <Nav.Link className="navtext" href="#music">MUSIC</Nav.Link>
+              <Nav.Link className="navtext" href="#tour">TOUR</Nav.Link>
+              <Nav.Link className="navtext" href="#merch">MERCH</Nav.Link>
+              {/* <NavDropdown alignRight title="Inspiration" id="collasible-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Youtube Poop Playlist</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Musical Inspiration</NavDropdown.Item>
+              </NavDropdown> */}
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
     )
   }
 }
