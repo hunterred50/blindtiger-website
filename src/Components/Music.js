8888efd542059ac27d38ec183385e20754f36945
Hunter Redding <hunterred50@gmail.com>
2020-01-21 22:15:07 -0500

    deployment test successful

--- src/Components/Music.js
+++ src/Components/Music.js
@@ -6,7 +6,7 @@ class Music extends Component {
     return (
       // <Transition>
       <div className="container" id="music" height="1000px">
-        <h1>Music?</h1><p></p>
+        <h1>Music</h1><p></p>
       <iframe title="Lock the Gate" src="https://open.spotify.com/embed/album/2K8GFA0sQhnHFAZQxvgDeF" width="75%" height="390" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
       <p></p>
       <iframe title="Waterfall" src="https://open.spotify.com/embed/album/2QZUdZ6SubPpVqcCfxZLTd" width="75%" height="235" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
