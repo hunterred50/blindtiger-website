99d28d084d921b4295c000e233583451008ac2af
Hunter Redding <hunterred50@gmail.com>
2020-01-21 22:26:11 -0500

    added padding in components

--- src/Components/Music.js
+++ src/Components/Music.js
@@ -5,11 +5,11 @@ class Music extends Component {
   render() {
     return (
       // <Transition>
-      <div className="container" id="music" height="1000px">
+      <div className="container" id="music" style={{paddingBottom: '30px'}}>
         <h1>Music</h1><p></p>
-      <iframe title="Lock the Gate" src="https://open.spotify.com/embed/album/2K8GFA0sQhnHFAZQxvgDeF" width="75%" height="390" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
-      <p></p>
-      <iframe title="Waterfall" src="https://open.spotify.com/embed/album/2QZUdZ6SubPpVqcCfxZLTd" width="75%" height="235" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+        <iframe title="Lock the Gate" src="https://open.spotify.com/embed/album/2K8GFA0sQhnHFAZQxvgDeF" width="75%" height="390" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+        <p></p><p></p>
+        <iframe title="Waterfall" src="https://open.spotify.com/embed/album/2QZUdZ6SubPpVqcCfxZLTd" width="75%" height="235" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
       </div>
       // </Transition>
     )
