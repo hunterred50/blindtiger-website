3f6d7c2e4552f16b177904f52b66e9dc227c5099
Hunter Redding <hunterred50@gmail.com>
2020-02-03 16:09:26 -0500

    random name generation in chat adjusted

--- src/Components/Chat.js
+++ src/Components/Chat.js
@@ -6,30 +6,16 @@ import Input from "./Chat/Input";
 
 function randomName() {
   const adjectives = [
-    "autumn", "hidden", "bitter", "misty", "silent", "empty", "dry", "dark",
-    "summer", "icy", "delicate", "quiet", "white", "cool", "spring", "winter",
-    "patient", "twilight", "dawn", "crimson", "wispy", "weathered", "blue",
-    "billowing", "broken", "cold", "damp", "falling", "frosty", "green", "long",
-    "late", "lingering", "bold", "little", "morning", "muddy", "old", "red",
-    "rough", "still", "small", "sparkling", "throbbing", "shy", "wandering",
-    "withered", "wild", "black", "young", "holy", "solitary", "fragrant",
-    "aged", "snowy", "proud", "floral", "restless", "divine", "polished",
-    "ancient", "purple", "lively", "nameless"
+    "blonde", "blatant", "blackened", "blasted", "blamable", "bleached",
+    "blind", "blended", "blithe", "blissful", "blunt", "blushing", "bluecoat", 
+    "blue", "poopoo"
   ];
   const nouns = [
-    "waterfall", "river", "breeze", "moon", "rain", "wind", "sea", "morning",
-    "snow", "lake", "sunset", "pine", "shadow", "leaf", "dawn", "glitter",
-    "forest", "hill", "cloud", "meadow", "sun", "glade", "bird", "brook",
-    "butterfly", "bush", "dew", "dust", "field", "fire", "flower", "firefly",
-    "feather", "grass", "haze", "mountain", "night", "pond", "darkness",
-    "snowflake", "silence", "sound", "sky", "shape", "surf", "thunder",
-    "violet", "water", "wildflower", "wave", "water", "resonance", "sun",
-    "wood", "dream", "cherry", "tree", "fog", "frost", "voice", "paper", "frog",
-    "smoke", "star"
+    "tiger", "toner", "toiger", "toffee", "tibber", "peepee"
   ];
   const adjective = adjectives[Math.floor(Math.random() * adjectives.length)];
   const noun = nouns[Math.floor(Math.random() * nouns.length)];
-  return adjective + noun;
+  return adjective + " " + noun;
 }
 
 function randomColor() {
