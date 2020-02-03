c8885802b61088c77278b17a24a6b0ac113d25da
Hunter Redding <hunterred50@gmail.com>
2020-02-03 16:18:23 -0500

    Vid/music resize

--- src/Components/Music.js
+++ src/Components/Music.js
@@ -7,9 +7,9 @@ class Music extends Component {
       // <Transition>
       <div className="container" id="music"style={{paddingTop: '70px'}}>
         <h1>Music</h1><p></p>
-        <iframe title="Lock the Gate" src="https://open.spotify.com/embed/album/2K8GFA0sQhnHFAZQxvgDeF" width="75%" height="390" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+        <iframe title="Lock the Gate" src="https://open.spotify.com/embed/album/2K8GFA0sQhnHFAZQxvgDeF" width="100%" height="390" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
         <p></p><p></p>
-        <iframe title="Waterfall" src="https://open.spotify.com/embed/album/2QZUdZ6SubPpVqcCfxZLTd" width="75%" height="235" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+        <iframe title="Waterfall" src="https://open.spotify.com/embed/album/2QZUdZ6SubPpVqcCfxZLTd" width="100%" height="235" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
       </div>
       // </Transition>
     )
--- src/Components/Vids.js
+++ src/Components/Vids.js
@@ -13,13 +13,13 @@ class Vids extends Component {
       <div className="container" id="vids" style={{paddingBottom: '70px'}}>
         <h1>Vids</h1>
         <p style={{paddingTop:'20px'}}></p>
-        <iframe title="inTheFuture" width="75%" height="315" src="https://www.youtube.com/embed/XLlM3NzrcKU" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen />
+        <iframe title="inTheFuture" width="100%" height="315" src="https://www.youtube.com/embed/XLlM3NzrcKU" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen />
         <p style={{paddingTop:'20px'}}></p>
-        <iframe title="littleRoom2018" width="75%" height="315" src="https://www.youtube.com/embed/MkTeVukUPdw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen />
+        <iframe title="littleRoom2018" width="100%" height="315" src="https://www.youtube.com/embed/MkTeVukUPdw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen />
         <p style={{paddingTop:'20px'}}></p>
-        <iframe title="whatIsThis" width="75%" height="315" src="https://www.youtube.com/embed/_bOE20PcIfI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen />
+        <iframe title="whatIsThis" width="100%" height="315" src="https://www.youtube.com/embed/_bOE20PcIfI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen />
         <p style={{paddingTop:'20px'}}></p>
-        <iframe title="littleRoom2015" width="75%" height="315" src="https://www.youtube.com/embed/m1k8TS0Nsjo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen />
+        <iframe title="littleRoom2015" width="100%" height="315" src="https://www.youtube.com/embed/m1k8TS0Nsjo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen />
 
       </div>
     )
